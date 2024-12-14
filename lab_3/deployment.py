60e650155b518dfc4a24497d536ae4700bd4a7f0
danylo-maksymiv <danylo.maksymiv.shi.2023@lpnu.ua>
2024-12-14 08:19:25 +0200

    let me die v9

--- lab_3/deployment.py
+++ lab_3/deployment.py
@@ -3,6 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is not set in environment variables")
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
