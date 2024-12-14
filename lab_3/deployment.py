19b5b9d6cb800046920261879c7a4afab2db58f9
danylo-maksymiv <danylo.maksymiv.shi.2023@lpnu.ua>
2024-12-14 11:49:35 +0200

    let me die v16

--- lab_3/deployment.py
+++ lab_3/deployment.py
@@ -2,9 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ['SECRET']
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY is not set in environment variables")
+# SECRET_KEY = os.environ['SECRET']
+# if not SECRET_KEY:
+#     raise ValueError("SECRET_KEY is not set in environment variables")
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
@@ -20,9 +20,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blockchain', 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # MySQL Connection String
 conn_str = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
@@ -30,10 +34,10 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
-        'PORT': conn_str_params.get('port', '3306'),
+        'NAME': 'blockchain',
+        'HOST': 'blockchain-server.mysql.database.azure.com',
+        'USER': 'gtrajjyroa',
+        'PASSWORD': 'y$OZ15AqK$fznAgD',
+        'PORT': '3306',
     }
 }
